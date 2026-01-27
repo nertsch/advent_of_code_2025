40b45d1cf274d520678f375241c49b9065d96b41
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2026-01-27 23:50:08 +0100

    part_07 unify parts

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,69 +1,111 @@
 use std::collections::HashMap;
-pub fn part_1() -> u32 {
-    let mut map = read_input();
+use std::ptr::fn_addr_eq;
 
-    let start_x = map[0].iter().enumerate().find(|c| *c.1 == 'S').unwrap().0;
-    trace_beam_and_get_splits(&mut map, 0, start_x)
-}
-
-fn trace_beam_and_get_splits(map: &mut Vec<Vec<char>>, tip_y: usize, tip_x: usize) -> u32 {
-    if tip_y >= map.len() - 1 {
-        return 0;
-    }
-    match map[tip_y + 1][tip_x] {
-        '|' => return 0,
-        '.' => {
-            map[tip_y + 1][tip_x] = '|';
-            return trace_beam_and_get_splits(map, tip_y + 1, tip_x);
-        }
-        '^' => {
-            return 1
-                + trace_beam_and_get_splits(map, tip_y, tip_x - 1)
-                + trace_beam_and_get_splits(map, tip_y, tip_x + 1);
-        }
-        _ => panic!(),
-    }
+pub fn part_1() -> u64 {
+    get_possible_path_count(true)
 }
 pub fn part_2() -> u64 {
+    get_possible_path_count(false) + 1
+}
+
+fn get_possible_path_count(skip_overlapping_paths: bool) -> u64 {
     let mut map = read_input();
 
-    let start_x = map[0].iter().enumerate().find(|c| *c.1 == 'S').unwrap().0;
-    let mut result_cache = HashMap::new();
-    trace_beam_and_get_possible_path_count(&mut map, 0, start_x, &mut result_cache) + 1
+    let start = *map.iter().find(|c| *c.1 == Field::Start).unwrap().0;
+    let max_y = map.iter().map(|e| e.0.0).max().unwrap() + 1;
+    trace_beam_and_get_possible_path_count(
+        &mut map,
+        start.0,
+        start.1,
+        max_y,
+        skip_overlapping_paths,
+    )
 }
 
 fn trace_beam_and_get_possible_path_count(
-    map: &mut Vec<Vec<char>>,
+    map: &mut HashMap<(usize, usize), Field>,
     tip_y: usize,
     tip_x: usize,
-    result_cache: &mut HashMap<(usize, usize), u64>,
+    max_y: usize,
+    skip_overlapping_paths: bool,
 ) -> u64 {
-    if tip_y >= map.len() - 1 {
+    if tip_y >= max_y {
         return 0;
     }
 
-    if let Some(&value) = result_cache.get(&(tip_y, tip_x)) {
-        return value;
-    }
-
-    let result = match map[tip_y + 1][tip_x] {
-        '|' => 0,
-        '.' => trace_beam_and_get_possible_path_count(map, tip_y + 1, tip_x, result_cache),
-        '^' => {
-            1 + trace_beam_and_get_possible_path_count(map, tip_y, tip_x - 1, result_cache)
-                + trace_beam_and_get_possible_path_count(map, tip_y, tip_x + 1, result_cache)
+    let result = match map.get(&(tip_y + 1, tip_x)) {
+        Some(Field::Visited {
+            possible_path_count: ppc,
+        }) => {
+            if skip_overlapping_paths {
+                0
+            } else {
+                *ppc
+            }
+        }
+        None => trace_beam_and_get_possible_path_count(
+            map,
+            tip_y + 1,
+            tip_x,
+            max_y,
+            skip_overlapping_paths,
+        ),
+        Some(Field::Splitter) => {
+            1 + trace_beam_and_get_possible_path_count(
+                map,
+                tip_y,
+                tip_x - 1,
+                max_y,
+                skip_overlapping_paths,
+            ) + trace_beam_and_get_possible_path_count(
+                map,
+                tip_y,
+                tip_x + 1,
+                max_y,
+                skip_overlapping_paths,
+            )
         }
         _ => panic!(),
     };
-    result_cache.insert((tip_y, tip_x), result);
+    map.insert(
+        (tip_y, tip_x),
+        Field::Visited {
+            possible_path_count: result,
+        },
+    );
     result
 }
 
-fn read_input() -> Vec<Vec<char>> {
+#[derive(PartialEq, Eq)]
+enum Field {
+    Start,
+    Splitter,
+    Visited { possible_path_count: u64 },
+}
+
+fn read_input() -> HashMap<(usize, usize), Field> {
     let input = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/input_files/day_07.txt"
     ));
 
-    input.lines().map(|l| l.chars().collect()).collect()
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|(_, c)| *c != '.')
+                .map(move |(x, c)| {
+                    (
+                        (y, x),
+                        match c {
+                            'S' => Field::Start,
+                            '^' => Field::Splitter,
+                            _ => panic!(),
+                        },
+                    )
+                })
+        })
+        .collect()
 }
